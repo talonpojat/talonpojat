2e14fc861ddbe0e11923089b8a604f94fa19f370
Jouni Kähkönen <jouni@flexware.fi>
2026-05-18 15:09:36 +0300

    Lisätty PWA tuki ja asennusohjeet

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "talonpojat-cache-v1";
+
+const urlsToCache = [
+    "./",
+    "./index.html",
+    "./logo.png",
+    "./youtube_logo.png",
+    "./icon-192.png",
+    "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+        .then(cache => {
+
+            return cache.addAll(urlsToCache);
+
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+        .then(response => {
+
+            return response || fetch(event.request);
+
+        })
+    );
+});
